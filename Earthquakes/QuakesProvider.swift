145da3b4ed69f4060f54a07e185fa924d470eaf5
Sam Lee <cable8mm@gmail.com>
2023-12-14 22:48:49 +1300

    View client data

---
+++ Earthquakes/QuakesProvider.swift
@@ -0,0 +1,27 @@
+//
+//  QuakesProvider.swift
+//  Earthquakes
+//
+//  Created by samgu.lee on 14/12/23.
+//
+
+import Foundation
+
+class QuakesProvider: ObservableObject {
+    @Published var quakes: [Quake] = []
+
+    let client: QuakeClient
+
+    func fetchQuakes() async throws {
+        let latestQuakes = try await client.quakes
+        self.quakes = latestQuakes
+    }
+
+    func deleteQuakes(atOffsets offsets: IndexSet) {
+        quakes.remove(atOffsets: offsets)
+    }
+    
+    init(client: QuakeClient = QuakeClient()) {
+        self.client = client
+    }
+}
